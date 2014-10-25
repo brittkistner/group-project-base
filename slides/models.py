b91c56cdc4605960a6c8b317a903a078fae9a10d
Brittney Kistner <bkistner01@gmail.com>
2014-10-25 11:59:41 -0700

    Set up Amazon S3 Storage for user images and comment attachments

--- slides/models.py
+++ slides/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='user_photo', blank=True, null=True)
+    name = models.CharField(max_length=255)
+
+class Attachment(models.Model):
+    pass
+
+class Comment(models.Model):
+    attachments = models.ManyToManyField(Attachment, related_name="comments", blank=True, null=True)
+    text = models.TextField()
+    user = models.ForeignKey(User, related_name="comments")
+    date = models.DateTimeField(auto_now_add=True)
+
