098598243ee244c343278c3a729d3ecbc19b734d
Garrett Kadillak <g.kadillak@comcast.net>
2014-10-28 01:02:16 -0700

    Styling, jquery done for searching.

--- slides/static/js/search.js
+++ slides/static/js/search.js
@@ -3,9 +3,30 @@
  */
 
 $(document).ready(function () {
-   $('#search').click(function () {
+
+    // Hide weekly slides, insert search box
+    $('#search').click(function () {
+       var $allweeks = $('#all-weeks');
         $('.accordion').slideUp('step');
-        $('#all-weeks').children(':last').slideToggle('easing');
-        $('#all-weeks').append()
-   });
+        $allweeks.children(':last').slideToggle();
+        $allweeks.append('<input type="search" id="search-box" placeholder="Enter search term here">');
+    });
+
+    // Remove
+    $('dt').click(function () {
+        var $all_weeks = $('#all-weeks');
+        var $this = $(this);
+        $all_weeks.children().hide();
+        $all_weeks.append('<h2 id="front-search">' + $this.text() + '</h2>');
+        $('.accordion').children().hide();
+
+        // If the order doesn't matter, just append the elements with a map
+        $('.reveal ul').css({
+            'list-style-type': 'none',
+            'margin-left': '-2%'
+        });
+
+        // Find the 'li' elements and replace them with paragraphs with text from inside previous tag
+//        $this.find('li').replaceWith('<p>' + $(this).find('li').text() + '</p>');
+    });
 });
