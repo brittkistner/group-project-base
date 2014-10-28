3b0e9a1d00f17ffc23adf4919db2b120c03214d8
Garrett Kadillak <g.kadillak@comcast.net>
2014-10-28 14:52:49 -0700

    Search page styled. Buttons need to be put in for slides/comment selector.

--- slides/static/js/search.js
+++ slides/static/js/search.js
@@ -12,21 +12,28 @@ $(document).ready(function () {
         $allweeks.append('<input type="search" id="search-box" placeholder="Enter search term here">');
     });
 
-    // Remove
+    $('#search-box').click(function() {
+       var $allweeks = $('#all-weeks');
+
+        $allweeks.append('<')
+    });
+
+    // Remove items not clicked on, put clicked title on line and show the accordion elements below
     $('dt').click(function () {
         var $all_weeks = $('#all-weeks');
         var $this = $(this);
+
         $all_weeks.children().hide();
         $all_weeks.append('<h2 id="front-search">' + $this.text() + '</h2>');
         $('.accordion').children().hide();
-
-        // If the order doesn't matter, just append the elements with a map
         $('.reveal ul').css({
             'list-style-type': 'none',
             'margin-left': '-2%'
         });
-
-        // Find the 'li' elements and replace them with paragraphs with text from inside previous tag
-//        $this.find('li').replaceWith('<p>' + $(this).find('li').text() + '</p>');
     });
+
+
+
+
+
 });
