690d656dff15ebbe8cceefc5b50fc92330328188
chintamanil <chintamani.lonkar@gmail.com>
2014-10-29 13:55:40 -0700

    working

--- slides/admin.py
+++ slides/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from slides.models import Comment, User
 # from rocketu.items import RocketuItem
 
 admin.site.register(User)
 admin.site.register(Comment)
 # admin.site.register(RocketuItem)
-=======
 from slides.models import Comment, User, Attachment
 
 admin.site.register(User)
 admin.site.register(Comment)
 admin.site.register(Attachment)
->>>>>>> 24ea56b98701ebc8cf327194fc0e35970e63b2f6
--- slides/models.py
+++ slides/models.py
@@ -9,38 +9,11 @@ class User(AbstractUser):
     def __unicode__(self):
         return self.name
 
-<<<<<<< HEAD
-
-class Attachment(models.Model):
-    file = models.ImageField(upload_to='media/comment_attachment', blank=True, null=True)
-    # add guid
-
-    def __unicode__(self):
-        return self.pk
-
-
-class Comment(models.Model):
-    attachments = models.ForeignKey(Attachment, related_name="comments", blank=True, null=True)
-    text = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User, related_name="comments")
-    date = models.DateTimeField(auto_now_add=True)
-    week_number = models.IntegerField(blank=True, null=True)
-    day = models.CharField(max_length=5, blank=True, null=True)
-    slide_set = models.IntegerField(blank=True, null=True)
-    slide_number = models.IntegerField(blank=True, null=True)
-
-    def __unicode__(self):
-        return u"slide from week{}/{}/#/{}/{}".format(self.week_number, self.day, self.slide_set, self.slide_number)
 
 # https://students.rocketu.com/week8/5_am/#/1/2
 # https://students.rocketu.com/weekweek_number/day/#/slide_set/slide_number
 
 
-
-
-
-
-=======
 class Slide(models.Model):
     week_number = models.IntegerField()
     day = models.CharField(max_length=5)
@@ -70,4 +43,3 @@ class Attachment(models.Model):
 
     def __unicode__(self):
         return self.file.name.split('/')[2]  #FIX and add test
->>>>>>> 24ea56b98701ebc8cf327194fc0e35970e63b2f6
